451b4de6589bcc2da22136552f94ac7e121de1b3
unknown <anshmakwe@gmail.com>
2022-03-29 22:29:18 +0530

    initial commit

---
+++ copyright_algorithm.py
@@ -0,0 +1,26 @@
+s1 = "the sun rises in the east"
+
+s2 = "the sun in the sky is bright"
+
+T = set()
+
+for x in s1:
+    T.add(x)
+for x in s2:
+    T.add(x)
+
+print(T)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
